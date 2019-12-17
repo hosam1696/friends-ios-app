c6bb824f9f868c86494597bfcb113fd3c50d8ba4
hosamsam <hosamelnabawy1996@gmail.com>
2019-12-17 13:45:10 -0800

    Init project

--- swootch/ViewController.swift
+++ swootch/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    
+    @IBAction func pop(segue: UIStoryboardSegue) {
+        
+    }
 
 }
 
---
+++ swootch/views/BorderedButton.swift
@@ -0,0 +1,46 @@
+//
+//  BorderedButton.swift
+//  swootch
+//
+//  Created by Hosam Elnabawy on 12/17/19.
+//  Copyright © 2019 Hosam Elnabawy. All rights reserved.
+//
+
+import UIKit
+
+
+@IBDesignable
+class BorderedButton: UIButton {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setup()
+    }
+    
+    func setup() {
+        layer.borderWidth = 3.0
+        layer.borderColor = UIColor.white.cgColor
+        layer.cornerRadius = 12
+    }
+    
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
